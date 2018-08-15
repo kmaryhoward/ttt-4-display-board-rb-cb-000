6e08b030bfe21fc375943c28f086a953da3220c4
Kathleen Howard <kmaryhoward@gmail.com>
2018-08-15 21:44:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board(board)
   row_1 = " #{board[0]} | #{board[1]} | #{board[2]} "
-  row_2 = " #{board[3]} | #{board[4]} | #{board[5]} " 
-  row_3 = " #{board[6]} | #{board[7]} | #{board[8]} " 
+  row_2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row_3 = " #{board[6]} | #{board[7]} | #{board[8]} "
   line = "-----------"
   print row_1 + "\n" + line + "\n"+ row_2 + "\n" + line + "\n" + row_3
 end
